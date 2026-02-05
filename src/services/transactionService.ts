b8778b43446669949fe8301cf472af3ff85c65c5
Sayeed Tahseen <sayeedtahseen@Sayeeds-MBP.attlocal.net>
2026-02-04 23:35:28 -0500

    added env variables, and removed unused code

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -1,10 +1,16 @@
 import type { TransactionPayload } from "../models/TransactionPayloadModel";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+if (!API_BASE_URL) {
+  throw new Error("VITE_API_BASE_URL is not defined");
+}
+
 // transactionService.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: API_BASE_URL,
 });
 
 export function createTransaction(transaction: TransactionPayload) {
