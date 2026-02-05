459059591e8a133f10c7bc9c3e58398483b60a96
Sayeed Tahseen <sayeedtahseen@Sayeeds-MBP.attlocal.net>
2026-02-04 23:57:07 -0500

    updated url variable name

--- src/services/transactionService.ts
+++ src/services/transactionService.ts
@@ -1,9 +1,9 @@
 import type { TransactionPayload } from "../models/TransactionPayloadModel";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 if (!API_BASE_URL) {
-  throw new Error("VITE_API_BASE_URL is not defined");
+  throw new Error("VITE_API_URL is not defined");
 }
 
 // transactionService.js
